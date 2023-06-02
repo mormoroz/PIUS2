eee670351371a5433ff30d165b174b27bfad0461
Alexander Morozov <Alexander95223@yandex.ru>
2023-06-02 21:13:56 +0300

    feat(MySQL): added seeder and fix page for products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -17,16 +17,15 @@
                                 <div class="card-body">
                                     <div class="product_title">
                                         <p class="h2">{{$product->product_name}}</p>
-                                        <p class="h5 text-muted">{{$product->code}}</p>
                                     </div>
                                     <div class="border rounded-1 bg p-1 my-4 bg-cc-lighter">
                                         <div class="row">
-                                            <p class="h4"><strong>Каллории:</strong><span class="h4 m-2"><strong>{{$product->description}}</strong></span></p>
-                                            <p class="h6">Белки:<span class="h6 m-2">{{$product->price}}</span></p>
-                                            <p class="h6">Жиры:<span class="h6 m-2">{{$product->count}}</span></p>
+                                            <p class="h4"><strong>Код:</strong><span class="h4 m-2"><strong>{{$product->code}}</strong></span></p>
+                                            <p class="h6">Цена:<span class="h6 m-2">{{$product->price}}</span></p>
+                                            <p class="h6">Количество:<span class="h6 m-2">{{$product->count}}</span></p>
                                         </div>
                                     </div>
-                                    <button class="btn btn-outline-primary btn-sm stretched-link" data-bs-toggle="modal" data-bs-target="#addModal" data-bs-productID="{{$product->product_id}}">Добавить</button>
+                                    <button class="btn btn-outline-primary btn-sm stretched-link" data-bs-toggle="modal" data-bs-target="#addModal" data-bs-productID="{{$product->product_id}}">Подробнее</button>
                                 </div>
                             </div>
                         </div>
@@ -36,42 +35,42 @@
                 @endunless
             </div>
         </div>
-        <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-fullscreen-sm-down">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Добавить</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form action="/meal" method="post" >
-                            @csrf
-                            <input type="hidden" id="product_id" name="product_id">
-                            <div class="mb-3">
-                                <label for="mass_factor" class="col-form-label">Количество (в граммах):</label>
-                                <input type="text" class="form-control" id="mass_factor" name="mass_factor">
-                            </div>
-                            <div class="mb-3">
-                                <select class="form-select form-select-sm" aria-label=".form-select-sm example" name="meal_time">
-                                    <option selected="">Время приема</option>
-                                    <option value="0">Завтрак</option>
-                                    <option value="1">Обед</option>
-                                    <option value="2">Ужин</option>
-                                </select>
-                            </div>
-                            <div class="mb-3">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
-                                <button type="submit" class="btn btn-primary">Добавить</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+{{--        <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">--}}
+{{--            <div class="modal-dialog modal-dialog-centered modal-fullscreen-sm-down">--}}
+{{--                <div class="modal-content">--}}
+{{--                    <div class="modal-header">--}}
+{{--                        <h5 class="modal-title" id="exampleModalLabel">Добавить</h5>--}}
+{{--                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>--}}
+{{--                    </div>--}}
+{{--                    <div class="modal-body">--}}
+{{--                        <form action="/meal" method="post" >--}}
+{{--                            @csrf--}}
+{{--                            <input type="hidden" id="product_id" name="product_id">--}}
+{{--                            <div class="mb-3">--}}
+{{--                                <label for="mass_factor" class="col-form-label">Количество (в граммах):</label>--}}
+{{--                                <input type="text" class="form-control" id="mass_factor" name="mass_factor">--}}
+{{--                            </div>--}}
+{{--                            <div class="mb-3">--}}
+{{--                                <select class="form-select form-select-sm" aria-label=".form-select-sm example" name="meal_time">--}}
+{{--                                    <option selected="">Время приема</option>--}}
+{{--                                    <option value="0">Завтрак</option>--}}
+{{--                                    <option value="1">Обед</option>--}}
+{{--                                    <option value="2">Ужин</option>--}}
+{{--                                </select>--}}
+{{--                            </div>--}}
+{{--                            <div class="mb-3">--}}
+{{--                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>--}}
+{{--                                <button type="submit" class="btn btn-primary">Добавить</button>--}}
+{{--                            </div>--}}
+{{--                        </form>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
     </div>
 
     </div>
-
+{{--    <img class="card-img-top" src="{{ Vite::asset('resources/icons/iPhone-9.png') }}">--}}
     @vite(['resources/js/products.index.js'])
 
 @endsection
